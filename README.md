eb061a8f74562d4ba8bf182235d6c2710d8695ab
William Hales <will@d-three.co.uk>
2024-02-06 17:42:26 +0700

    Development for free mailing list. Code interacts with Google Sheets and can collect form data.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mailing List 
+
+This is an open sourced product that allows you to simply create a page capable of collecting user data for a mailing list. 
+
+TODO: 
+
+ - Replace with netlify function and use HTMX
+ - Need to create netlify function to integrate with Google instead. I need to inject the script URL in an env variable for security and this can't be done in vanilla js. 
+ - Need to create a template for the Google Sheet file. Anyone should be able to easily create this app. 
---
+++ netlify/functions/signup.mjs
@@ -0,0 +1,35 @@
+const axios = require('axios');
+
+export default async (req, context) => {
+    const scriptURL = process.env.GOOGLE_SHEET_URL;
+
+    try {
+        const requestBody = await req.json();
+    
+        if (!requestBody || !requestBody.data || !requestBody.data.name || !requestBody.data.email) {
+            return new Response("Please provide name and email data in the request body.", { status: 400 });
+        }
+    
+        const name = requestBody.data.name;
+        const email = requestBody.data.email;
+    
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+    
+        const response = await axios.post(scriptURL, formData);
+    
+        if (response.status === 200) {
+            return new Response("Thanks ☺️");
+        } else {
+            throw new Error('Failed to submit form');
+        }
+    } catch (error) {
+        if (error instanceof SyntaxError && error.message === 'Unexpected end of JSON input') {
+            return new Response("Invalid JSON format in the request body.", { status: 400 });
+        } else {
+            console.error('Error!', error.message);
+            return new Response("Error occurred during form submission.", { status: 500 });
+        }
+    }
+}
